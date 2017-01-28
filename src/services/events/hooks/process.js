0adae6d25efc6af6ff5db89e19a3065868eb7117
Kemal Yenilmez <info@gazatem.com>
2017-01-28 16:41:37 +0200

    Fields updated

--- src/services/events/hooks/process.js
+++ src/services/events/hooks/process.js
@@ -7,21 +7,34 @@
 
 module.exports = function(options) {
   return function(hook) {
-    const clientId = hook.data.client.clientId.substring(0, 50).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
-    const apiKey = hook.data.client.apiKey.substring(0, 50).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
-    const productId = hook.data.product.prodid.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
-    const productPrice = hook.data.product.price;
+    const client_id = hook.data.client.client_id.substring(0, 50).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+    const api_key = hook.data.client.api_key.substring(0, 50).replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+    
+    const product_id = hook.data.product.id.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
+    const product_price = hook.data.product.price;
+    const product_model = hook.data.product.model;
+    const product_name = hook.data.product.name;
+
+    const customer_name = hook.data.customer.name;
+    const customer_id = hook.data.customer.id;
+    const customer_email = hook.data.customer.email;
+    const customer_phone = hook.data.customer.phone;
+
     const referrer = hook.data.headers.referrer.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
     const documentURI = hook.data.headers.uri.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
     const userAgent = hook.data.headers.agent.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
-    const customerId = hook.data.customer.customerId;
     // Override the original data
     hook.data = {
-        apiKey,
-        clientId,
-        productId,
-        productPrice,
-        customerId,
+        client_id,
+        api_key,
+        product_id,
+        product_name,
+        product_model,
+        product_price,
+        customer_id,
+        customer_name,
+        customer_email,
+        customer_phone,
         referrer,
         documentURI,
         userAgent,
